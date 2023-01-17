09b80450c8d50619a7dd465277ee5af96819bde3
alon suissa <alonsuissa12@gmail.com>
2023-01-17 02:01:26 +0200

    part A working!!!!

--- sniffer.c
+++ sniffer.c
@@ -10,51 +10,58 @@
 #include "arpa/inet.h"
 
 typedef struct application_header {
-    unsigned int unix_time;
-    unsigned short total_length;
-    unsigned char reserved: 3;
-    unsigned char cache: 1;
-    unsigned char steps: 1;
-    unsigned char type: 1;
-    unsigned short status_code: 10;
-    unsigned short cache_control;
-    unsigned short padding;
-    unsigned char data[8180];
+    uint32_t unix_time;
+    uint16_t total_length;
+    union {
+        uint16_t flags ;
+        uint16_t reserved: 3;
+        uint16_t cache: 1;
+        uint16_t steps: 1;
+        uint16_t type: 1;
+        uint16_t status_code: 10;
+    };
+
+    uint16_t cache_control;
+    uint16_t padding;
 } app_header;
 
 void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
     FILE *pfile;
     pfile = fopen("211344015_208007351.txt", "a+");
-    printf("cdbclbl");
     if (pfile == NULL) {
         printf("Error opening file!\n");
         return;
     }
 
     struct ether_header *eth_header = (struct ether_header *) packet;
-    struct tcphdr *tcp_header = (struct tcphdr *) (packet + sizeof(struct ether_header) +
-                                                   sizeof(struct ip)); // thats it?
     const struct ip *ip_header = (struct ip *) (packet + sizeof(struct ether_header));
-    app_header *appH = (app_header *) (packet + sizeof(struct ether_header) + sizeof(struct ip) + sizeof(struct tcphdr));
+    struct tcphdr *tcp_header = (struct tcphdr *) (packet + sizeof(struct ether_header) + (ip_header->ip_hl) * 4);
+    app_header *appH = (app_header *) (packet + sizeof(struct ether_header) + (ip_header->ip_hl) * 4 +
+                                       tcp_header->doff * 4);
 
 
-    char source_ip[INET_ADDRSTRLEN], dest_ip[INET_ADDRSTRLEN], *data;
+    char source_ip[INET_ADDRSTRLEN], dest_ip[INET_ADDRSTRLEN];
     inet_ntop(AF_INET, &ip_header->ip_src, source_ip, INET_ADDRSTRLEN);
     inet_ntop(AF_INET, &ip_header->ip_dst, dest_ip, INET_ADDRSTRLEN);
-    unsigned int source_port =  ntohs(tcp_header->th_sport);
-    unsigned int dest_port = ntohs(tcp_header->th_dport);
-    unsigned int timestamp = appH->unix_time;
-    unsigned int total_length =  ntohs(header->caplen);
-    unsigned char cache_flag = appH->cache;
-    unsigned char steps_flag = appH->steps;
-    unsigned char type_flag = appH->type;
-    unsigned int status_code = appH->status_code;
-    unsigned short cache_control = appH->cache_control;
+    //
+    unsigned char *data = (unsigned char *) (packet + sizeof(struct ether_header) + (ip_header->ip_hl) * 4 + tcp_header->doff * 4 + sizeof(app_header));
+    unsigned int data_size = header->len - sizeof(struct ether_header) - (ip_header->ip_hl) * 4 - tcp_header->doff * 4 ;
+    if(tcp_header->psh) {
+        fprintf(pfile,"source ip: %s, dest_ip: %s\n",source_ip, dest_ip);
+        fprintf(pfile,"source_port: %u, dest_port: %hu\n",  ntohs(tcp_header->th_sport), ntohs(tcp_header->th_dport));
+        fprintf(pfile,"timestamp: %u\ntotal_length: %hu\n", ntohl(appH->unix_time), ntohs(appH->total_length));
+        fprintf(pfile, "cache Flag: %u\n", (ntohs(appH->flags) >> 12)  & 0x1) ;
+        fprintf(pfile, "steps Flag: %u\n", (ntohs(appH->flags) >> 11) & 0x1);
+        fprintf(pfile, "type Flag: %u\n", (ntohs(appH->flags) >> 10)  & 0x1);
+        fprintf(pfile, "status Code: %u\n", ntohs(appH->flags) & ((1 << 10) - 1));
+        fprintf(pfile, "cache_control: %hu\ndata:", ntohs(appH->cache_control));
 
-    fprintf(pfile,"%u\n",(unsigned int)header->ts.tv_sec);
-    fprintf(pfile,
-            "source ip: %s, dest_ip: %s, source_port: %u, dest_port: %u, timestamp: %u, total_length: %u, cache_flag: %hu, steps_flag: %u, type_flag: %u, status_code: %u, cache_control: %hu, data: \n",
-            source_ip, dest_ip, source_port, dest_port, timestamp, total_length, cache_flag, steps_flag, type_flag, status_code, cache_control);
+        for (int i = 0; i < data_size; i++) {
+            if (!(i & 15)) fprintf(pfile, "\n%04X:  ", i);
+            fprintf(pfile, "%02X ", ((unsigned char *) data)[i]);
+        }
+    }
+    fprintf(pfile,"\n");
 
     fclose(pfile);
 }
@@ -73,7 +80,7 @@ int main() {
 
     // Step 2: Compile filter_exp into BPF psuedo-code
     printf("1.5\n");
-    pcap_compile(handle, &fp, filter_exp, 0, net); // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    pcap_compile(handle, &fp, filter_exp, 0, net);
     printf("1.7\n");
     pcap_setfilter(handle, &fp);
     printf("2\n");
@@ -84,3 +91,12 @@ int main() {
     return 0;
 }
 
+//    uint16_t source_port = tcp_header->th_sport
+//    uint16_t dest_port = tcp_header->th_dport
+//    uint32_t timestamp = ntohl(appH->unix_time);
+//    uint16_t total_length = ntohl((header->caplen));
+//    uint16_t cache_flag = appH->cache;
+//    uint16_t steps_flag = appH->steps;
+//    uint16_t type_flag = appH->type;
+//    uint16_t status_code = appH->status_code;
+//    uint16_t cache_control = appH->cache_control;
